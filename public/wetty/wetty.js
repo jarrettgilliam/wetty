cf1fbaa17c779b402e2689733af36cb194aa1d63
Jarrett Gilliam <jarrettgilliam@gmail.com>
2016-08-23 19:41:29 -0500

    Change the color scheme

--- public/wetty/wetty.js
+++ public/wetty/wetty.js
@@ -39,6 +39,34 @@ socket.on('connect', function() {
         term.prefs_.set('scrollbar-visible', false);
         term.prefs_.set('audible-bell-sound', '')
 
+        // https://gist.github.com/johnbender/5018685
+        // Disable bold.
+        term.prefs_.set('enable-bold', false)
+
+        // Use this for Solarized Dark
+        term.prefs_.set('background-color', "#002b36");
+        term.prefs_.set('foreground-color', "#839496");
+
+        term.prefs_.set('color-palette-overrides', [
+          '#073642',
+          '#dc322f',
+          '#859900',
+          '#b58900',
+          '#268bd2',
+          '#d33682',
+          '#2aa198',
+          '#eee8d5',
+          '#002b36',
+          '#cb4b16',
+          '#586e75',
+          '#657b83',
+          '#839496',
+          '#6c71c4',
+          '#93a1a1',
+          '#fdf6e3'
+        ]);
+
+
         term.runCommandClass(Wetty, document.location.hash.substr(1));
         socket.emit('resize', {
             col: term.screenSize.width,
